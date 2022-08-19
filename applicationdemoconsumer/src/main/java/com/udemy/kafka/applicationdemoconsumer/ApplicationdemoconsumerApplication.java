33ca4ca140e07fad24db6b6f081dc84020b639b5
diegoschn <dlcschneider1@gmail.com>
2022-08-19 13:12:12 -0300

    refactoring project producer and consumer

---
+++ applicationdemoconsumer/src/main/java/com/udemy/kafka/applicationdemoconsumer/ApplicationdemoconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.kafka.applicationdemoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationdemoconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationdemoconsumerApplication.class, args);
+	}
+
+}
---
+++ applicationdemoconsumer/src/test/java/com/udemy/kafka/applicationdemoconsumer/ApplicationdemoconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.kafka.applicationdemoconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationdemoconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
